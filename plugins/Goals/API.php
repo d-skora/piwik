2c77f9d48aa5e91cf94a78cea90cf95938e60c11
d-skora <d.skora@clearcode.cc>
2014-11-24 17:23:19 +0100

    added getTranslationForMatchAttribute to goals API

--- plugins/Goals/API.php
+++ plugins/Goals/API.php
@@ -570,4 +570,32 @@ class API extends \Piwik\Plugin\API
             $this->renameNotDefinedRow($dataTable, $notDefinedStringPretty);
         }
     }
+    
+    public function getTranslationForMatchAttribute($match)
+    {
+        switch ($match) {
+            case 'manually':
+                return Piwik::translate('Goals_ManuallyTriggeredUsingJavascriptFunction');
+
+            case 'url':
+                return Piwik::translate('Goals_VisitUrl');
+
+            case 'title':
+                return Piwik::translate('Goals_VisitPageTitle');
+
+            case 'event_category':
+            case 'event_action':
+            case 'event_name':
+                return Piwik::translate('Goals_SendEvent');
+
+            case 'file':
+                return Piwik::translate('Goals_Download');
+
+            case 'external_website':
+                return Piwik::translate('Goals_ClickOutlink');
+
+            default:
+                return '';
+        }
+    }
 }
